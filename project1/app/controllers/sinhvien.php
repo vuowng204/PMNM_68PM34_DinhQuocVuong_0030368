d16dd68da95eeacc1b8252b716e9823277bf2f9a
vuowng204 <vuong8g@gmail.com>
2026-05-15 01:24:32 +0700

    URL process

---
+++ project1/app/controllers/sinhvien.php
@@ -0,0 +1,11 @@
+<?php
+    class sinhvien 
+    {
+        public function index(){
+            echo "<h1>Trang chu sinh vien</h1>";
+        }
+        public function about(){
+            echo "<h1>Trang about sinh vien</h1>";
+        }
+    }
+?>
